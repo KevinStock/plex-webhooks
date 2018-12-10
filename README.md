d97e517c7adf23328184f6c26e9e7c893cf9ac5f
Kevin Stock <kevin@kevinstock.net>
2018-12-09 19:11:14 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# plex-webhooks
+
+## Description
+A node.js application that will control LIFX lights through Plex Webhooks.
+
+The color of the lights will be determined by the dominant color of media image for the media that is being played back.
+
+Different behaviors are enabled for different rooms but for all rooms, the lights are dimmed to 25% when playing and increased to 100% when paused.
+
+## Setup
+
+1. `npm run setup`
+  - Copies the `.sample_env` file to `.env`
+2. Modify the `.env` file to update the appropriate variables.
+  - `PLEXADDRESS` is your plex server URL
+  - `PLEXTOKEN` is your Plex Authentication Token
+  - `LIFXAUTH` is your LIFX Authentication Token
+  - `APPLETVS` is a key/value pair for your AppleTVs and their UUID (keys and key order should match the `LIFXGROUPIDS` keys)
+  - `LIFXGROUPIDS` is a key/value pair for your LIFX Bulb Group IDs (keys and key order should match the `APPLETVS` keys)
+3. `npm install`
+  - Install all dependencies
+4. `npm run start`
+  - Runs the application
+
+## Dependencies
+1. express
+2. multer
+3. request
+4. img-color
---
+++ plexWebhooks.js
@@ -0,0 +1,121 @@
+require('dotenv').load();
+// Dependencies
+var express = require('express'),
+  request = require('request'),
+  multer = require('multer'),
+  color = require('img-color'),
+  app = express(),
+  upload = multer({
+    dest: '/tmp/'
+  });
+
+// Apple TVs
+var APPLETVS = {'LivingRoom': 'EB5D43ED-416A-4C96-AC21-997582561DBD',
+            'Bedroom': '18114917-2071-4486-BDFF-0B87897A8655',
+            'Basement': '73E643FA-7653-4D77-977E-44859DFDF491'}
+
+// LIFX Bulb Groups
+var LIFXGROUPIDS = {'LivingRoom': '258662f1abbffed5d7410280860b9eef',
+                'Bedroom': '93cfa6a8b2266c52e55e5c7c18b3ac44',
+                'Basement': '603d06fdac6d5390eb80d58fcf4e9861'}
+
+
+console.log('Listening...');
+
+app.post('/', upload.single('thumb'), function(req, res, next) {
+  var payload = JSON.parse(req.body.payload);
+  console.log('Got webhook for', payload.event);
+
+  // Get Media Details
+  var mediaTitle = payload.Metadata.grandparentTitle;
+  var mediaImage = process.env.PLEXADDRESS + payload.Metadata.grandparentThumb + '?X-Plex-Token=' + process.env.PLEXTOKEN;
+  // Log Player ID
+  console.log('Player ID: ' + payload.Player.uuid + ' (' + getKeyByValue(APPLETVS, payload.Player.uuid) + ')');
+
+  // Actions for Apple TVs
+  if (isAppleTV(payload.Player.uuid) && payload.Metadata.type != 'track') {
+    var light_group = LIFXGROUPIDS[getKeyByValue(APPLETVS, payload.Player.uuid)];
+    var options = {
+      method: 'PUT',
+      json: true,
+      url: 'https://api.lifx.com/v1/lights/group_id:' + light_group + '/state',
+      headers: {
+         'Authorization': process.env.LIFXAUTH
+      }
+    }
+
+    // Media is Started
+    if (payload.event == 'media.play' || payload.event == 'media.resume') {
+      // Turn light off.
+      console.log('Media Played: Turning lights down.');
+      options.body = {
+        "power": "on",
+        "brightness": 0.25
+      };
+      color.getDominantColor(mediaImage)
+        .then(function(col) {
+          options.body.color = "#" + col.dColor;
+          request(options);
+        })
+        .catch(err => console.error(err));
+    }
+
+    // Media Paused
+    else if (payload.event == 'media.pause') {
+      // Turn light on.
+      console.log('Media Paused: Turning lights up.');
+      options.body = {
+        "power": "on",
+        "brightness": 1.0
+      };
+      color.getDominantColor(mediaImage)
+        .then(function(col) {
+          options.body.color = "#" + col.dColor;
+          request(options);
+        })
+        .catch(err => console.error(err));
+
+    }
+
+    // Media Stopped
+    else if (payload.event == 'media.stop') {
+      // Only turn the lights on if in the Living Room
+      if (getKeyByValue(APPLETVS, payload.Player.uuid) == 'LivingRoom') {
+        color = "white";
+        console.log('Media Stopped: Turning lights up.');
+        options.body = {
+          "power": "on",
+          "color": "white",
+          "brightness": 1.0
+        }
+      }
+      else {
+        console.log('Media Stopped: Turning lights off.');
+        options.body = {
+          "color": "white",
+          "brightness": 1.0,
+          "power": "off"
+        }
+      }
+    request(options);
+    }
+  }
+  res.sendStatus(200);
+
+  // Function to check if Player is an AppleTV
+  function isAppleTV(uuid) {
+    for (appleTV in APPLETVS){
+      if (APPLETVS[appleTV] == uuid) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Function to get Key by Value from associative array
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+});
+
+app.listen(3100);
